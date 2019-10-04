a5624771a08bf50182d73ce041b7f351e67cdb1b
RaniaMahmoud <rosyblack88@yahoo.com>
2019-10-04 10:48:29 +0200

    first commit

---
+++ FP1/ArgsEcho/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgsEcho
+{
+    class ArgsEcho
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Received the following arguments:\n");
+            //Console.WriteLine(args[0]);
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"[{i}] = {args[i]}");
+            }
+
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FP1/FP1/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace FP1
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+
+        //    //WriteLine("Hey!!!");
+
+        //    //string s = ReadLine();
+        //    //WriteLine(int.Parse(s)+10);
+        //    //int s1 = 10;
+        //    //WriteLine(s1.ToString() + 10);
+
+        //    Process p = new Process();
+        //    p.StartInfo.FileName = "d.txt";
+        //    p.StartInfo.Arguments = "param1 param2";
+        //    p.Start();
+        //    p.WaitForExit();
+        //    WriteLine(args[0]);
+        //    ReadKey();
+        //}
+        static void Main()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("ArgsEcho.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+          
+            startInfo.Arguments = "adsdfgh";
+            Process.Start(startInfo);
+
+        }
+    }
+}
