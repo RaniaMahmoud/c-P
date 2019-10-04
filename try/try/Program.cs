bdade0a97e64ddaae8d7909a285f5f0001da5220
RaniaMahmoud <rosyblack88@yahoo.com>
2019-10-04 11:09:03 +0200

    first commit

---
+++ try/try/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @try
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Doc:Marwan mahmoud :)");
+            ReadKey();
+        }
+    }
+}
